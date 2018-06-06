493f82b2f1bc8f7bf21493781a87049730cc6295
Arslan <arslanaziz@appsgenii.eu>
2018-06-06 17:13:21 +0500

    Save post using SetData
    get post data using class http

--- Blog/Controller/Post/Submit.php
+++ Blog/Controller/Post/Submit.php
@@ -12,8 +12,9 @@ use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\View\Result\PageFactory;
-//use Magento\Framework\Controller\Result\JsonFactory;
-use Kvr\Blog\Model\Post;
+use Kvr\Blog\Model\PostFactory;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\App\Request\Http;
 
 class Submit extends Action
 {
@@ -23,62 +24,65 @@ class Submit extends Action
     protected $resultPageFactory;
 
     /**
-     * @var Magento\Framework\Controller\Result\JsonFactory;
-     */
-    //protected $resultJsonFactory;
-
-    /**
-     * @var Kvr\Blog\Model\Post;
-     */
-    protected $post;
-
-    /**
      * @var Magento\Framework\Controller\ResultFactory
      */
     protected $resultFactory;
 
     /**
-     * @param Context     $context
+     * @var Magento\Framework\App\Request\Http
+     */
+    protected $request;
+
+    /**
+     * @var Kvr\Blog\Model\PostFactory
+     */
+     protected $postFactory;
+
+    /**
+     * @param Context $context
      * @param PageFactory $resultPageFactory
      */
     public function __construct(
         Context $context,
         PageFactory $resultPageFactory,
-        //JsonFactory $resultJsonFactory,
         ResultFactory $resultFactory,
-        Post $post
+        PostFactory $postFactory,
+        //PostResource $postResource,
+        Http $request
     )
     {
 
         $this->resultPageFactory = $resultPageFactory;
-        //$this->resultJsonFactory = $resultJsonFactory;
-        $this->post = $post;
-        $this->ResultFactory = $resultFactory;
+        $this->resultFactory = $resultFactory;
+        $this->postFactory = $postFactory;
+        //$this->postResource = $postResource;
+        $this->request = $request;
         return parent::__construct($context);
     }
 
     public function execute()
     {
-       $resultPage = $this->resultPageFactory->create();
-        $title = $this->getRequest()->getPostValue("title");
-        $content = $this->getRequest()->getPostValue("content");
-        echo $title;
-        echo "<br />";
-        echo $content;
-        echo "<br />";
-        //exit;
-        //$post = $this->getRequest()->getPostValue();
-        //$data=array('title'=>$post['title'],'content'=>$post['content']);
-        //$data=array('title'=>'title123','content'=>'content123');
-        $post = $this->post;
+        $resultPage = $this->resultPageFactory->create();
 
-        $post->setTitle($title);
-        $post->setContent($content);
-        $post->save();
-        $this->messageManager->addSuccess(__('Your values has beeen submitted successfully.'));
+        //Save Data using setData Method using post resource model
+        $postData = $this->request->getPost();
+            //Probably best do some form of validation here but for tutorial purposes, we wont for now
+            $title = $postData['title'];
+            $issue = $postData['content'];
 
-        $redirect = $this->ResultFactory->create(ResultFactory::TYPE_REDIRECT);
-        $redirect->setUrl('/blog/index/index');
-        return $redirect;
+            $data = array('title'=>$title,'content'=>$issue);
+            $postResource = $this->postFactory->create();
+
+            try {
+                $postResource->setData($data);
+                $postResource->save();
+                $this->messageManager->addSuccess("New Post: $title Created");
+                $redirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+                $redirect->setUrl('/blog/index/index');
+                return $redirect;
+            } catch (\Exception $e) {
+                //Add a error message if we cant save the new note from some reason
+                $this->messageManager->addError("Unable to save this Post: $title ");
+            }
     }
 }
