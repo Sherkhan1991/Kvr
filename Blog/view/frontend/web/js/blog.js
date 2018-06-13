e5691f81ea9cfc3540765ee116576ac2908cfe48
Arslan <arslanaziz@appsgenii.eu>
2018-06-13 17:03:34 +0500

    Post Update - Ajax Multi request issue Fix
       $('#blogSubmit').click(function(e) {
          $('<input />').attr('type', 'hidden')
              .attr('name', "edit")
              .attr('value', 1)
              .appendTo('#blogSubmit');
          return true;
      });
      //Ajax Data Multiple items
      data: { key1: 'value1', key2: 'value2' }
      
      Current Follow:
      https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
      
      Ajax Option: http://api.jquery.com/jquery.ajax/
      
      
      Errors Fix
      500 (Internal Server Error) 
      Uncaught TypeError: Illegal invocation
      
      Solution:
      contentType: false
      processData: false
      
      Error:
      Json response fields undefined
      solution:
      var form = $('#blogPost')[0];

--- Blog/view/frontend/web/js/blog.js
+++ Blog/view/frontend/web/js/blog.js
@@ -1,17 +1,32 @@
 require(['jquery','mage/url'], function($, url){
-    var customurl = url.build('../../blog/post/submit');
-    console.log(customurl);
     //Pub Static path of module
     //var imageUrl = require.toUrl('Kvr_Blog/images/loading.gif');
     //var imageModel = require.toUrl('pub/media/kvr/loading.gif');
     //$('.loading').html(imageUrl);
     $('#blogSubmit').click(function(e) {
         e.preventDefault();
+        var customurl;
+        var formData;
 
+        //Use For View Template
+        if($(this).hasClass('Update')) {
+            console.log('View');
+            var form = $('#blogPost')[0];
+            formData = new FormData(form);
+            formData.append('edit', 1);
+            customurl = url.build('blog/post/view');
+            console.log(formData);
+        } else {
+            console.log('Submit');
+            formData = $('#blogPost');
+            customurl = url.build('blog/post/submit');
+        }
         $.ajax({
             type: 'POST',
             url: customurl,
-            data: $('#blogPost').serialize(),
+            data: formData,
+            processData: false,
+            contentType: false,
             beforeSend:function()
             {
                 $('#blogPost').hide();
@@ -21,7 +36,17 @@ require(['jquery','mage/url'], function($, url){
                 console.log(JSON.stringify(msg));
                 $('#blogPost').show();
                 $('.loading').hide();
-                $( "#ajaxData" ).load( url.build('../../blog/index/index') + " #ajaxData" );
+
+                if($(this).hasClass('Update'))
+                {
+                    $("#ajaxData").load(customurl + " #ajaxData");
+                    console.log('Success');
+                } else {
+                    console.log('Edit Success');
+                    console.log(msg['edit']);
+                    console.log(msg['postid']);
+                }
+
             }
         });
 
