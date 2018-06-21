29c1a199ecfc079c1cdb7b9e90f6fa882df21cbd
Arslan <arslanaziz@appsgenii.eu>
2018-06-21 18:24:57 +0500

    Admin Interface - Menu
    Displayed Menu in System
    access at admin/blog/posts/index

---
+++ Blog/Controller/Adminhtml/Posts/Index.php
@@ -0,0 +1,35 @@
+<?php
+namespace Kvr\Blog\Controller\Adminhtml\Posts;
+
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/blog_posts_index.xml
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        return  $resultPage = $this->resultPageFactory->create();
+    }
+}
