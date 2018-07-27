b782065fb877c919e1fa797312e52a5bd42deb48
Arslan <arslanaziz@appsgenii.eu>
2018-07-27 18:38:17 +0500

    Admin Interface UI Component - Error Fix
    2 issues Fixed Now Posts Displaying at backend

--- Blog/Model/Post.php
+++ Blog/Model/Post.php
@@ -18,6 +18,13 @@ use Kvr\Blog\Api\Data\PostInterface;
  */
 class Post extends AbstractModel implements PostInterface, IdentityInterface
 {
+    /**#@+
+     * Post's Statuses
+     */
+    const STATUS_ENABLED = 1;
+    const STATUS_DISABLED = 0;
+    /**#@-*/
+
     /**
      * Cache tag
      */
@@ -49,6 +56,16 @@ class Post extends AbstractModel implements PostInterface, IdentityInterface
         $this->_init('Kvr\Blog\Model\ResourceModel\Post');
     }
 
+    /**
+     * Prepare post's statuses.
+     * Available event blog_post_get_available_statuses to customize statuses.
+     *
+     * @return array
+     */
+    public function getAvailableStatuses()
+    {
+        return [self::STATUS_ENABLED => __('Enabled'), self::STATUS_DISABLED => __('Disabled')];
+    }
 
     /**
      * Get Title
